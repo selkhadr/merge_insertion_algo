19c4c6ccd760b391d0dd78f43d97f0bfe142720f
saraGithu <sara.elkhadri@usmba.ac.ma>
2023-12-11 10:00:53 +0100

    first pushh

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,7 +4,16 @@ int main(int ac, char **av)
 {
     PmergeMe pmergeMe;
 
+    if (ac <= 2)
+    {
+        std::cout << "please enter arguments" << std::endl;
+        return 1;
+    }
     pmergeMe.fill_vector(ac, av);
     pmergeMe.merge(ac, av);
     pmergeMe.insertion();
+    std::cout << " \t\t\t\tmain_chain" << std::endl;
+    pmergeMe.print_vect(pmergeMe.main_chain);
+    std::cout << " \t\t\t\tpend" << std::endl;
+    pmergeMe.print_vect(pmergeMe.pend);
 }
